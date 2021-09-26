c26e4f3dd5b0b182b5805310b1b0f090870fead5
Souhail El fchtali <sel-fcht@e0r2p5.1337.ma>
2021-09-26 16:24:51 +0100

    MINI

---
+++ Makefile
@@ -0,0 +1,6 @@
+norme:
+	norminette *.c *.h
+push:
+	git add .
+	git commit -m "MINI"
+	git push
---
+++ minishell.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-fcht <sel-fcht@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/26 16:15:26 by sel-fcht          #+#    #+#             */
+/*   Updated: 2021/09/26 16:24:40 by sel-fcht         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int main()
+{
+    //TO DO LIST :
+        // parsing command from the input line 
+        // lexer -> parser -> execution 
+        // nsewl shi wlad l97ab 
+}
