74d9b02b418d008d4a860329f1f0cad38198099b
Souhail El fchtali <sel-fcht@e0r2p5.1337.ma>
2021-10-09 11:30:35 +0100

    gnl

--- minishell.c
+++ minishell.c
@@ -6,16 +6,13 @@
 /*   By: sel-fcht <sel-fcht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 16:15:26 by sel-fcht          #+#    #+#             */
-/*   Updated: 2021/09/26 16:24:40 by sel-fcht         ###   ########.fr       */
+/*   Updated: 2021/10/08 12:17:28 by sel-fcht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main()
+int main(int ac, char **av)
 {
-    //TO DO LIST :
-        // parsing command from the input line 
-        // lexer -> parser -> execution 
-        // nsewl shi wlad l97ab 
+    return(0);
 }
--- minishell.h
+++ minishell.h
@@ -3,5 +3,15 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define BUFFER_SIZE 10000
+#define SYMBOL_1 //;
+#define SYMBOL_2 // &
+#define SYMBOL_3 // <
+#define SYMBOL_4 // >
+#define SYMBOL_5 // |
 
+char *delimi = ";&<>|";
+
+int         get_next_line(int fd, char **line);
+char        *ft_substr(char *s, unsigned int start, size_t len);
 #endif 
