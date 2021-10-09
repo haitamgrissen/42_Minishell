49514f3081b56aca95d7f8e97f6de5b102b93d2c
Souhail El fchtali <sel-fcht@e0r2p5.1337.ma>
2021-10-09 13:11:41 +0100

    MINI

--- minishell.c
+++ minishell.c
@@ -6,13 +6,57 @@
 /*   By: sel-fcht <sel-fcht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 16:15:26 by sel-fcht          #+#    #+#             */
-/*   Updated: 2021/10/08 12:17:28 by sel-fcht         ###   ########.fr       */
+/*   Updated: 2021/10/09 13:10:30 by sel-fcht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void parse(char *str)
+{
+    t_shell *shell;
+    
+    shell->first = (char *)malloc(ft_strlen(str) + 1);
+    int i;
+    i = 0;
+    while(str[i]!= '\0' && str[i] != ' ')
+    {
+        shell->first[i] = str[i];
+        i++;
+    }
+    shell->first[i] = '\0';
+    i = 0;
+    while(shell->first[i] != '\0')
+    {
+        write(1,&shell->first[i],1);
+        write(1,"\n",1);
+        i++;
+    }
+}
+
+
+char *ft_putstr(char *str)
+{
+    int i;
+    i = 0;
+    while (str[i])
+    {
+        write(1,&str[i],1);
+        i++;
+    }
+    return (str);
+}
 int main(int ac, char **av)
 {
+    t_shell *sh;
+    sh = malloc(sizeof(t_shell));
+    sh->str = ft_strdup("");
+    printf("%s",sh->str);
+    while(1)
+    {
+        write(1, "Minishell $>",12);
+        get_next_line(0, &sh->str);
+        parse(sh->str);
+    }
     return(0);
 }
--- minishell.h
+++ minishell.h
@@ -9,9 +9,22 @@
 #define SYMBOL_3 // <
 #define SYMBOL_4 // >
 #define SYMBOL_5 // |
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+typedef struct shell
+{
+    char *str;
+    char *first; // first command that has been entered " ls -al"  "first = ls" in this case
 
-char *delimi = ";&<>|";
+}t_shell;
 
 int         get_next_line(int fd, char **line);
 char        *ft_substr(char *s, unsigned int start, size_t len);
+int         outtil(int fd, char **rest, char **line);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_strdup(char *s1);
+char		*kayn_ster(char *str);
+int			ft_strlen(char *s);
+
 #endif 
