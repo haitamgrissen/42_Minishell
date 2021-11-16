598019a82b3bf9adb4d34b4bdc7b60aff4b01a87
Souhail El fchtali <sel-fcht@e2r13p3.1337.ma>
2021-11-16 13:39:36 +0100

    leak fixed hh

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: sel-fcht <sel-fcht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 20:39:02 by hgrissen          #+#    #+#             */
-/*   Updated: 2021/11/16 10:29:55 by sel-fcht         ###   ########.fr       */
+/*   Updated: 2021/11/16 13:37:29 by sel-fcht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,20 @@ int	main(int ac, char **av, char **env)
 	(void)av;
 	init_envs(env);
 	refresh_bash();
+
 	if (ac == 1)
 	{
 		while (TRUE)
 		{
 			catch_the_signal();
 			g_exe.line = readline("BASH$>: ");
-			if (!g_exe.line)
+			if (g_exe.line == NULL)
 			{
 				ft_putstr_fd("exit\n", 1);
-				return (0);
+				exit (0);
 			}
 			if (g_exe.line[0] == '\0')
-				continue ;
+				;
 			add_history(g_exe.line);
 			lexer = init_lexer(g_exe.line);
 			parse(lexer);
@@ -39,6 +40,8 @@ int	main(int ac, char **av, char **env)
 				execute_pipe(g_exe.cmd);
 			free(lexer);
 			refresh_bash();
+			//free(g_exe.line);
+			system("leaks minishell");
 		}
 	}
 	return (0);
